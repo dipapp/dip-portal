b355d2dd6ef3e83bcca92be2aef22ae09e0efe12
dipapp <admin@dipmembers.com>
2025-08-11 15:57:13 -0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,2 +1,12 @@
-import Navbar from "@/components/Navbar";
-import "./globals.css";
+'use client';
+import Link from 'next/link';
+
+function Navbar() {
+  return (
+    <nav style={{padding:'12px 16px', borderBottom:'1px solid #eee'}}>
+      <Link href="/">DipMembers</Link>
+    </nav>
+  );
+}
+
+export default Navbar;   // <-- add this
